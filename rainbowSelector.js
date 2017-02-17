b9cf2eaaa03bf812aaadc19c4528f46f5fae7cbb
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-02-17 15:40:33 -0500

    Working highlight change on mouseup

--- rainbowSelector.js
+++ rainbowSelector.js
@@ -1,4 +1,33 @@
+//Insert and get a reference to the new stylesheet
+var sheet = (function() {
+  console.log("Creating a sheet")
+  // Create the <style> tag
+  var style = document.createElement("style");
+
+  // WebKit hack :(
+  style.appendChild(document.createTextNode(""));
+
+  // Add the <style> element to the page
+  document.head.appendChild(style);
+
+  return style.sheet;
+})();
+
+get_color = function() {
+  return "352e7e";
+}
+
 $( "body" ).bind( "mouseup", function() {
   var selection = window.getSelection();
-  console.log(selection.toString());
+  var newHighlight = get_color(selection);
+  var selectors = ["::selection"]//, "::-mos-selection", "::-o-selection", "::-ms-selection", "::-webkit-selection"]
+  //Clean out old rules
+  while(sheet.rules.length > 0){
+    sheet.deleteRule(0);
+  }
+  //Write new rules
+  for(i = 0; i < selectors.length; i++){
+    sheet.insertRule(selectors[i] + "{ background-color: #" + newHighlight + "; color: #fff; }", 0 );
+  }
+
 });
