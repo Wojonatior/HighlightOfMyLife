c8cca6289f4866d4bedf4d9efc1911884d48a5e5
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-02-17 16:35:02 -0500

    complete logic flow, implementation bugs

--- rainbowSelector.js
+++ rainbowSelector.js
@@ -1,39 +1,49 @@
 //Insert and get a reference to the new stylesheet
 var sheet = (function() {
   console.log("Creating a sheet")
-  // Create the <style> tag
   var style = document.createElement("style");
-
   // WebKit hack :(
   style.appendChild(document.createTextNode(""));
-
-  // Add the <style> element to the page
   document.head.appendChild(style);
-
   return style.sheet;
 })();
 
+//Returns a hex color code based on weird rules from a string
 get_color = function(selection) {
   selection.replace(/[^a-f]/g , "0");
   selection = pad_nearest_three(selection);
-  return "352e7e";
+  return get_two_from_thirds(selection);
 }
 
+//get the first two characters from each third of the string
+//assumed to be passed a string where % 3 === 0
+get_two_from_thirds = function(str){
+  thirdLength = str.length / 3;
+  var result = str[0] + str[1];
+  result += str[thirdLength] + str[thirdLength + 1]
+  result += str[2 * thirdLength] + str[2 * thirdLength + 1]
+  return result;
+}
+
+//Pads the string on the right with 0s up to the nearest 3 with 6 as a minimum length
 pad_nearest_three = function(str){
-  //Rounds up to the nearest 3, with a minimum of 6
   var intendedLength = Math.max( Math.ceil(str.length/3)*3, 6);
   var paddingString = "000000";
   return (str + paddingString).substring(0, intendedLength);
 }
 
+//Binds the highlighing rule changes to releasing the mouse
 $( "body" ).bind( "mouseup", function() {
   var selection = window.getSelection();
+  if(selection.length === 0){return;}
   var newHighlight = get_color(selection);
   var selectors = ["::selection"]//, "::-mos-selection", "::-o-selection", "::-ms-selection", "::-webkit-selection"]
+
   //Clean out old rules
   while(sheet.rules.length > 0){
     sheet.deleteRule(0);
   }
+
   //Write new rules
   for(i = 0; i < selectors.length; i++){
     sheet.insertRule(selectors[i] + "{ background-color: #" + newHighlight + "; color: #fff; }", 0 );
